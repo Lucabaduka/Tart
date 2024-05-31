bd2862284732da46db93b87ef2d07e52d37506b4
Lucabaduka <luca@calref.ca>
2024-05-30 17:07:30 -0700

    Better footers

--- Sources/Templates/splash.php
+++ Sources/Templates/splash.php
@@ -100,14 +100,12 @@
   <footer class="is-flex center">
     <div class="media-footer is-flex">
       <figure class="image depth is-32x32">
-        <a href="https://calref.ca/"><img src="./Static/icon.png" alt="CalRef Logo" aria-label="Visit Calamity Refuge"></a>
+        <a href="https://calref.ca/"><img src="./Static/icon.svg" alt="CalRef Logo" aria-label="Visit Calamity Refuge"></a>
       </figure>
-      <div class="media-content px-1">
-        <p class="is-size-6">
+        <p class="media-content is-size-6 px-1">
           <span><a class="gold" href="https://calref.ca/">CalRef</a></span><span> © 2008-<?=date("Y")?></span>
           <span> | <a class="gold" href="https://calref.ca/donate">Support Our Servers</a></span>
         </p>
-      </div>
     </div>
   </footer>
 
