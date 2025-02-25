beec01cc591c0794618ae84578c87684e18f65e9
Lucabaduka <luca@calref.ca>
2025-02-24 21:01:15 -0800

    More consistent spacing

--- Public/Static/butter.js
+++ Public/Static/butter.js
@@ -7,28 +7,28 @@ var LinkIndex = 0;
 
 function feeder() {
 
-    // Serve the advertised link
-    document.getElementById('feeder').innerHTML = "<p>" + links[LinkIndex] + "</p><p>Remaining: " + (links.length - (LinkIndex+1)) + "</p>";
-    document.getElementById('feeder').href = "https://www.nationstates.net/nation=" + links[LinkIndex] + "#composebutton";
+  // Serve the advertised link
+  document.getElementById('feeder').innerHTML = "<p>" + links[LinkIndex] + "</p><p>Remaining: " + (links.length - (LinkIndex+1)) + "</p>";
+  document.getElementById('feeder').href = "https://www.nationstates.net/nation=" + links[LinkIndex] + "#composebutton";
 
-    // Disable the button if at the end of the list
-    if ( LinkIndex == links.length ){
-        document.getElementById('feeder').innerHTML = "<p>--Finished--</p><p>Hooray!</p>";
-        document.getElementById('feeder').href = "javascript: void(0)";
-        LinkIndex = links.length;
-    }
+  // Disable the button if at the end of the list
+  if ( LinkIndex == links.length ){
+    document.getElementById('feeder').innerHTML = "<p>--Finished--</p><p>Hooray!</p>";
+    document.getElementById('feeder').href = "javascript: void(0)";
+    LinkIndex = links.length;
+  }
 
-    // Otherwise, increment the count
-    else {
-        LinkIndex++;
-    }
+  // Otherwise, increment the count
+  else {
+    LinkIndex++;
+  }
 
 }
 
 // Turns things green when clicked by toggling the okay class
 // If it's already there, we don't want to turn it off again
 function okay(e) {
-    if (!e.classList.contains("okay")) {
-        e.classList.toggle("okay");
-    }
+  if (!e.classList.contains("okay")) {
+    e.classList.toggle("okay");
+  }
 }
